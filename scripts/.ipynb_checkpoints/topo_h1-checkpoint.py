62cd31297fb78892990769db380e7ae217b30a6a
Jose Gomez <gomezgaona@gmail.com>
2025-01-15 18:59:52 +0000

    1024 senders and receivers

--- scripts/.ipynb_checkpoints/topo_h1-checkpoint.py
+++ scripts/.ipynb_checkpoints/topo_h1-checkpoint.py
@@ -1,51 +1,77 @@
 #!/usr/bin/python
 
 import os
-import sys
 import subprocess
+import sys
 from mininet.net import Mininet
 from mininet.node import OVSKernelSwitch
 from mininet.cli import CLI
 
 def start_mininet_hosts(num_hosts, buffer_size):
-    if(num_hosts > 254):
-        print("You are trying to add more than 254 senders and receivers")
-        return
+    RED = "\033[31m"
+    RESET = "\033[0m"
     net = Mininet(topo=None, build=False, ipBase='172.16.0.0/16')
 
-    # Create switches
-    s1 = net.addSwitch('s1', cls=OVSKernelSwitch, failMode='standalone')
-    print("Creating switches")
+    host_sw = 32 # Hosts per switches
+    if num_hosts > host_sw:
+        num_sw = int(num_hosts / host_sw)
+    else:
+        num_sw = 2
+
+    # Creating switches
+    s_left = []
+    print("Creating left switches")
+    for i in range(0, num_sw):
+        s_left.append(net.addSwitch(f's_left{i+1}', cls=OVSKernelSwitch, failMode='standalone'))
     
-    # Start hosts and connect them to the switch
-    for i in range(1, num_hosts + 1):
-        # Senders (hs)
-        hs = net.addHost(f'hs{i}', ip=f'172.16.0.{i}/16')
+    # Aggregator switches 
+    print("Creating aggregator switches")
+    s_agg1 = net.addSwitch('s_agg1', cls=OVSKernelSwitch, failMode='standalone')
+    
+    # Connecting all the switches to the aggregator
+    print("Linking left switches to the aggregator switch")
+    for i in range(0, num_sw):
+        net.addLink(s_left[i], s_agg1)
+ 
+    # Creating hosts on the left
+    print("Creating left hosts")
+    for i in range(num_hosts):
+        third_octet = (i // 254) % 256
+        fourth_octet = (i % 254) + 1
+
+        ip_address = f'172.16.{third_octet}.{fourth_octet}/16'
+        hs = net.addHost(f'hs{i+1}', ip=ip_address)
+
+        net.addLink(hs, s_left[int(i/host_sw)])
         hs.cmd('sysctl -w net.ipv4.tcp_wmem="{} {} {}"'.format(buffer_size, buffer_size, buffer_size))
         hs.cmd('sysctl -w net.ipv4.tcp_rmem="{} {} {}"'.format(buffer_size, buffer_size, buffer_size))
-        net.addLink(hs, s1)
-        #print(i)
-    print(f"Creating hosts, setting TCP send and receive buffers to {buffer_size}, and connecting hosts to the switches")
 
+    print(f"{RED}Range of IP addresses of left hosts 172.16.0.1-172.16.{third_octet}.{fourth_octet} {RESET}")
+    print(f"Setting TCP send and receive buffers to {buffer_size} Mbytes")
+    
     # Start the network
     net.start()
     print("Starting the network")
-
+    
     # Open the Mininet CLI
     net.interact()
     
     # Clean up after the network has been stopped
     net.stop()
+    
 
-# Check if the user provided a parameter
-if len(sys.argv) > 1:
-    num_hosts = int(sys.argv[1])
+def main():
+    if len(sys.argv) > 1:
+        num_hosts = int(sys.argv[1])
+        if num_hosts > 1024:
+            print("The maximum number of hosts allowed is 1024")
+            exit()
+    else:
+        print("No arguments passed. Specify the number of hosts e.g., sudo python3 topo_h1.py 1024")
+        exit()
+
     TCP_buffer_size = "4096 1000000 200000000"
     start_mininet_hosts(num_hosts, TCP_buffer_size)
-else:
-    print("No parameter provided. Please enter a parameter.")
 
-
-
-
-
+if __name__ == "__main__":
+    main()
